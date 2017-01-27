1ce50e89f8fbd8c2054e410d67581e2cb2d1260b
chenshenqian <chenshenqian@meituan.com>
2017-01-27 14:08:49 +0800

    and

--- src/transduce.js
+++ src/transduce.js
@@ -4,41 +4,31 @@ var curryN = require('./curryN');
 
 
 /**
- * Initializes a transducer using supplied iterator function. Returns a single
- * item by iterating through the list, successively calling the transformed
- * iterator function and passing it an accumulator value and the current value
- * from the array, and then passing the result to the next call.
+ * 用迭代函数来初始化一个 transducer ，为集合中每个元素执行迭代函数进行 transform
+ * ，将返回值传入加法器，然后把累加传入进行下一轮迭代。
  *
- * The iterator function receives two values: *(acc, value)*. It will be
- * wrapped as a transformer to initialize the transducer. A transformer can be
- * passed directly in place of an iterator function. In both cases, iteration
- * may be stopped early with the `R.reduced` function.
+ * 迭代函数接收2个参数： *(acc, value)* ，然后被包装成 transformer 来初始化 transducer 。
+ * Transformer 能直接替代迭代函数。并且，打断 `R.transduce` 的迭代过程比 `R.reduced` 更容易。
  *
- * A transducer is a function that accepts a transformer and returns a
- * transformer and can be composed directly.
+ * Transducer 接收 transformer ，然后返回 transformer ，可以直接用于函数组合。
  *
- * A transformer is an an object that provides a 2-arity reducing iterator
- * function, step, 0-arity initial value function, init, and 1-arity result
- * extraction function, result. The step function is used as the iterator
- * function in reduce. The result function is used to convert the final
- * accumulator into the return type and in most cases is R.identity. The init
- * function can be used to provide an initial accumulator, but is ignored by
- * transduce.
+ * Transformer 传入2个参数到迭代函数进行迭代，传入0个参数到初始化函数进行初始化，传入1个参数到累加函数作为结果。
+ * 迭代用于 reduce 的迭代；
+ * 累加函数作为累加器，用来计算返回值，通常使用 `R.identity` ；
+ * 初始化函数用来计算初始值，但在 transduce 里面被忽略。
  *
- * The iteration is performed with R.reduce after initializing the transducer.
+ * 在 transduce 初始化之后，迭代方式和 `R.reduce` 一样。
  *
  * @func
  * @memberOf R
  * @since v0.12.0
  * @category List
  * @sig (c -> c) -> (a,b -> a) -> a -> [b] -> a
- * @param {Function} xf The transducer function. Receives a transformer and returns a transformer.
- * @param {Function} fn The iterator function. Receives two values, the accumulator and the
- *        current element from the array. Wrapped as transformer, if necessary, and used to
- *        initialize the transducer
- * @param {*} acc The initial accumulator value.
- * @param {Array} list The list to iterate over.
- * @return {*} The final, accumulated value.
+ * @param {Function} xf transducer 函数，接收 transformer 返回 transformer
+ * @param {Function} fn 迭代函数，累加器和数组当前位置的元素，被包装成 transformer ，如果必要的话将用于 transducer 的初始化
+ * @param {*} acc 累加器初始值
+ * @param {Array} list 迭代集合
+ * @return {*} 累加器的最终结果
  * @see R.reduce, R.reduced, R.into
  * @example
  *
