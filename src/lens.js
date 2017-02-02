ac09acead3d7eebf2a4640a6c49d52bf05890aff
wangzengdi <sunnyadi@163.com>
2017-02-02 15:49:19 +0800

    update src/lens.js by wangzengdi

--- src/lens.js
+++ src/lens.js
@@ -3,7 +3,7 @@ var map = require('./map');
 
 
 /**
- * 返回封装了给定 getter 和 setter 方法的 lens 。 getter 和 setter 分别用于 “获取” 和 “设置” 被 `lens` focus 的值。但 setter 不会改变传入值。
+ * 返回封装了给定 getter 和 setter 方法的 lens 。 getter 和 setter 分别用于 “获取” 和 “设置” 被 `lens` focus 的值。setter 不会改变原数据。
  *
  * @func
  * @memberOf R
